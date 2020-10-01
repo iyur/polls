061dd6acd63c8f52f343cb73701a2efd508debf9
Oleksii Solomakha <akamazz@gmail.com>
2020-10-01 15:56:01 +0300

    Revert "Fix query builder"
    This reverts commit e3c56a4d5a4f62d73f7d6b86e044139451fe4a95.

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -2,25 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Models\Poll_first;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\DB;
-use Illuminate\View\View;
-use Laravel\Lumen\Application;
 
 class PollController extends Controller
 {
 
-	private string $salt = 'P4nJ4jHr49SYtsbKHNuc44SA';
-
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @param Request $request
-	 * @return Response|View|Application
-	 */
+    private $salt = 'P4nJ4jHr49SYtsbKHNuc44SA';
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index(Request $request)
     {
         if (!empty($request->all()))
@@ -28,27 +21,27 @@ class PollController extends Controller
         return view('polls.poll_1.index', compact('response'));
     }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param $hash
-	 * @return Response|View|Application
-	 */
-    public function show(String $hash)
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($hash)
     {
-        $result = DB::select('SELECT * FROM polls WHERE hash = ? LIMIT 1', [$hash]);
+        $result = Poll_first::where('hash', $hash)->take(1)->get()[0];
         return view('polls.poll_1.view', compact('result'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param Request $request
-     * @return RedirectResponse
-	 */
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
-        $response = DB::insert('INSERT INTO polls (variant, hash) VALUES (?, ?)', [$request->variant, hash('sha256', date('Y-m-d H:i:s') . $this->salt)]);
+        $response = Poll_first::create(['variant' => $request->variant, 'hash' => hash('sha256', date('Y-m-d H:i:s') . $this->salt)]);
         return redirect()->route('poll_1.view', ['hash' => $response->hash]);
     }
 }
